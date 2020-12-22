58e56b5fa2e3ae5686d3ded198d58659b042d0f2
Franklin Collin Tamboto <tamboto2000@gmail.com>
2020-12-22 11:22:10 +0700

    Delete unused error types

--- error.go
+++ error.go
@@ -8,16 +8,8 @@ import "errors"
 // JSON delimiter (',', '}' or ']'), nor syntax char, then it must be invalid
 var errInvalid = errors.New("invalid")
 
-// occured when data type can't be determined.
-// Example, when the first char is a syntax char, like \n, \t, white space. etc. then it
-// can't be determined since the next char is not recognizable type
-// var errUnrecognizable = errors.New("unrecognizable")
-
 // occured when the called data parser method check the first char and it's not
 // the rigth parser to parse the data type.
 // Example, when parser method for parsing numeric type called and the first char is
 // not numeric, then it's not the parser job to parse the data
 var errUnmatch = errors.New("unmatch")
-
-// occured when reader is EOF
-// var errReaderEOF = errors.New("EOF")
